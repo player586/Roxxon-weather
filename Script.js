dbe7be1b033ed35c97a821f7b739c0fbb58c8622
player586 <elijah.gnadou@icloud.com>
2026-03-07 19:34:29 -0500

    Add async function to fetch weather data

---
+++ Script.js
@@ -0,0 +1,17 @@
+async function getWeather(){
+
+let city = document.getElementById("city").value;
+
+let apiKey = "YOUR_API_KEY";
+
+let url = "https://api.openweathermap.org/data/2.5/weather?q="
++ city + "&appid=" + apiKey + "&units=metric";
+
+let response = await fetch(url);
+
+let data = await response.json();
+
+document.getElementById("weather").innerText =
+"Temperature: " + data.main.temp + "°C";
+
+}
